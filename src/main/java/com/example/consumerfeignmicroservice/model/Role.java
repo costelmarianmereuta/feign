9e739f10664b685051f9ac0df735dafdb1049f37
costelmarianmereuta <mereuta.mariana1@gmail.com>
2019-11-01 19:06:56 +0100

    feign client without tests

---
+++ src/main/java/com/example/consumerfeignmicroservice/model/Role.java
@@ -0,0 +1,18 @@
+package com.example.consumerfeignmicroservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    private long id;
+    private String role;
+
+
+}
